e9699d8cb68aaccdb23981dabe89d4f5bd312b28
Damian Padilla <damipad4@gmail.com>
2022-09-02 20:43:52 -0500

    init js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get Quotes from API
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await
+    } catch(error) {
+        alert(error)
+        // Catch Error Here 
+    }
+}
